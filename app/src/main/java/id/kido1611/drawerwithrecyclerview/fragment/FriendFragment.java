eb37305c85ec440180bc4f18a303b288fc1aa37d
Muhammad Abdusy Syukur <aci@hotmail.co.id>
2016-04-22 18:24:27 +0700

    Initial commit

---
+++ app/src/main/java/id/kido1611/drawerwithrecyclerview/fragment/FriendFragment.java
@@ -0,0 +1,45 @@
+package id.kido1611.drawerwithrecyclerview.fragment;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import id.kido1611.drawerwithrecyclerview.R;
+
+/**
+ * Created by Kido1611 on 22-Apr-16.
+ */
+public class FriendFragment extends Fragment {
+    public FriendFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_friend, container, false);
+
+
+        // Inflate the layout for this fragment
+        return rootView;
+    }
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+    }
+}
